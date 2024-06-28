e3d28855ad5116b3fa41e9b78a6b9900192fdb8c
ReshabPrasad <153843058+ReshabPrasad@users.noreply.github.com>
2024-06-28 20:50:19 +0530

    draw check added

--- src/componenets/Grid/Grid.jsx
+++ src/componenets/Grid/Grid.jsx
@@ -7,10 +7,20 @@ import isWinner from "../helpers/isWinner";
 
 
 
+function isDraw(board){
+    for(let i=0;i<9;i++){
+        if(board[i]=="")return false;
+    }
+    return true;
+}
+
+
+
 function Grid({number}){
     const [turn,setturn]=useState(true);
     const [board,setboard]=useState(Array(number).fill(""));
     const [winner,setwinner]=useState(null);
+    const [draw,setdraw] = useState(false);
 
     function play(index){
         console.log("move played",index);
@@ -23,6 +33,11 @@ function Grid({number}){
         if(win){
             setwinner(win);
             toast.success(`Congratulation ${win} win the game`);
+        }
+        else if(isDraw(board)){
+            console.log(draw);
+            setdraw(true);
+            toast.warning(`The game is a draw`);
         }
         setturn(!turn);
         setboard([...board]);
@@ -37,6 +52,7 @@ function Grid({number}){
     return (
         <div className="grid-wrapper">
             {winner && (<h1 className="h1">Winner : {winner}</h1>)}
+            {!winner && draw && (<h1 className="h1">The game is a draw</h1>)}
             <div>
             <ToastContainer position="bottom-center"/>
             <h1 className="h1">TIC-TAC-TOE</h1>
